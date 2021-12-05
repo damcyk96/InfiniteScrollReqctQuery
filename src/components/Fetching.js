5ab083a03536513b651c7d3a867cd991a8932013
Damian Obajtek <damian.obajtek@gmail.com>
2021-12-05 13:39:36 +0100

    small fix

--- src/components/Fetching.js
+++ src/components/Fetching.js
@@ -6,27 +6,32 @@ const Fetching = () => {
   const [characters, setCharacters] = useState([]);
   const [page, setPage] = useState(1);
 
-  const fetchCharacters = async () => {
-    const res = await axios.get(
-      "https://rickandmortyapi.com/api/character/?page=" + page
-    );
-    
-    return res;
-  };
-  const { data, fetchNextPage, isLoading } = useInfiniteQuery(
-    "characters",
-    fetchCharacters
-  );
 
-  //usememo potrzebne do dodawania do arrayki z responsa
-
-  useEffect(() => {
-    if (data) {
-      setCharacters(data.pages[0].data.results);
+  const fetchCharacters = async ({ pageParam = 1 }) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${pageParam}`);
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
     }
-  }, [data, page]);
+    return response.json();
+  };
+  
+  const {
+    data,
+    isLoading,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    error
+  } = useInfiniteQuery('characters', fetchCharacters, {
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.page < lastPage.total_pages) return lastPage.page + 1;
+      return false;
+    }
+  });
 
-  // console.log(data.pages[0].data.results);
+  console.log(hasNextPage)
+
+
   if (isLoading) {
     return <h1>Loading</h1>;
   }
@@ -34,18 +39,16 @@ const Fetching = () => {
   return (
     <div>
       <h1>Próba infinite loadingu </h1>
-      <div>
+      {/* <div>
         {characters?.map((item) => (
           <div key={item.id}>
             <h3>{item.name}</h3>
             <img src={item.image} alt={item.name} />
           </div>
         ))}
-      </div>
+      </div> */}
       <button
-        onClick={() => {
-          setPage(prevPage => prevPage + 1);
-        }}
+        onClick={fetchNextPage}
       >
         Load more
       </button>
